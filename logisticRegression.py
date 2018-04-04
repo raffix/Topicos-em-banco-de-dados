3a7d8ceebb7a0575293d923c7474f9c232f535ee
Raffael C. Rossi <raffixrcr@hotmail.com>
2018-04-04 20:40:07 -0300

    Corrigido

--- logisticRegression.py
+++ logisticRegression.py
@@ -5,29 +5,30 @@ import matplotlib.pyplot as pl
 
 def RMSE(y, y_hat, m):
 	return np.sqrt((1/m) * (np.sum(y_hat - y) ** 2))
-
 ## returns the predicted y (based on feature dataset (X) and weights (Theta))
 def F(X,T):
     return X.dot(T.T)
+    
+def J(X, y, th):
+  m = X.shape[0]
+  y_hat = g(X, th)
+  l1 = y * np.log(y_hat)
+  l2 = (1 -y) * np.log(y_hat)
+  error = (l1 + l2).sum()
+  return -1 * (error/m)
 
-## returns the cost (residual error) for predicted y
-def J(X,y,T):
+def GD (X, y, th, alpha, niters):
   m=X.shape[0]
-  y_hat=X.dot(T.T)
-  cost=np.sum((y_hat-y)**2)/(2*m)
-  return cost
-
-## finds the "good" values for the weigths (Theta)
-## Gradient descent (optmization function)
-def GD (X,y,T,alpha,niters):
-  m=X.shape[0]
-  cost=np.zeros((niters,1))
+  cost = np.zeros((niters,1))
   for i in range(0,niters):
-    y_hat=X.dot(T.T)
-    error=((y_hat-y)*X).sum(0)/m
-    T=T-(alpha*(error))
-    cost[i]=J(X,y,T)
-  return T,cost
+    y_hat = X.dot(th.T)
+    error = ((y_hat-y)*X).sum(0)/m
+    th = th-(alpha*(error))
+    cost[i] = J(X,y,th)
+  return th,cost
+  
+def g(X, th):
+  return 1/ (1+ (np.e**(-(X.dot(th.T)))))
   
 def normalizeDataset(X, m, d):
 	return (X - np.min(X,0)) / (np.max(X, 0) - np.min(X,0))
@@ -50,9 +51,7 @@ if __name__ == "__main__":
     y=np.array([l[d:d+1] for l in rows],dtype=float) ## vector of dataset labels
     X=np.array([l[0:d-1] for l in rows],dtype=float) ## matrix of dataset features
     Theta=np.zeros((1,d)) ## Initialize Theta with zeros
-    
     X = normalizeDataset(X, m, d)
-    
     X=np.insert(X,0,1,axis=1) ## inserts a column of 1's
     tsize=int(m*0.7) ## size of the training set
     Xtr=X[:tsize,:] ## from the first row to tsize-1, all colmuns
@@ -65,14 +64,6 @@ if __name__ == "__main__":
     pl.plot(cost)
     pl.show()
     y_hat=F(Xte,Theta)
-    
-    print(RMSE(yte, yte.dot(Theta), m))
-    
-    for i in range(10):
-	    print(y_hat[i], '<-->', yte[i], '=>', np.abs(y_hat[i] - yte[i]))
-    if d>1:
-        print('Sorry, we cannot plot more than 1 feature')
-        exit(0)
     ## predict new labels from training set
     y_hat=Xtr.dot(Theta.T)
     ## Plot the predict y's in relation to the ground-truth (y)
@@ -88,8 +79,9 @@ if __name__ == "__main__":
     ## now X regarding the predict values
     pl.plot(Xte[:,1:],y_hat,'-',c='g')
     pl.show()
-    print("Test cost", J(Xte,yte,Theta))
+    ##print("Test cost", J(Xte,yte,Theta))
     pos = (y==1).ravel()
     neg = (y==0).ravel()
     pl.plot(X[pos,1], X[pos, 2], 'o', c='r')
     pl.plot(X[neg,1], X[neg, 2], 'o', c='b')
+    pl.show()
